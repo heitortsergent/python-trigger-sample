20e23ff50c3f032394db3f7a1b6bfad14641ac07
Steve Avery <savery@statestitle.com>
2017-11-03 15:02:40 -0700

    convert to python 3

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ def main():
 
         test_runs = trigger_json.get("runs", [])
 
-        print "Started {} test runs.".format(len(test_runs))
+        print("Started {} test runs.".format(len(test_runs)))
 
         results = {}
-        while len(results.keys()) < len(test_runs):
+        while len(list(results.keys())) < len(test_runs):
             time.sleep(1)
 
             for run in test_runs:
@@ -26,20 +26,20 @@ def main():
                     if result.get("result") in ["pass", "fail"]:
                         results[test_run_id] = result
 
-        pass_count = sum([r.get("result") == "pass" for r in results.values()])
-        fail_count = sum([r.get("result") == "fail" for r in results.values()])
+        pass_count = sum([r.get("result") == "pass" for r in list(results.values())])
+        fail_count = sum([r.get("result") == "fail" for r in list(results.values())])
 
         if fail_count > 0:
-            print "{} test runs passed. {} test runs failed.".format(pass_count, fail_count)
+            print("{} test runs passed. {} test runs failed.".format(pass_count, fail_count))
             exit(1)
 
-        print "All test runs passed."
+        print("All test runs passed.")
 
 
 def _get_result(test_run):
     # generate Personal Access Token at https://www.runscope.com/applications
     if not "RUNSCOPE_ACCESS_TOKEN" in os.environ:
-        print "Please set the environment variable RUNSCOPE_ACCESS_TOKEN. You can get an access token by going to https://www.runscope.com/applications"
+        print("Please set the environment variable RUNSCOPE_ACCESS_TOKEN. You can get an access token by going to https://www.runscope.com/applications")
         exit(1)
 
     API_TOKEN = os.environ["RUNSCOPE_ACCESS_TOKEN"]
@@ -51,7 +51,7 @@ def _get_result(test_run):
         "test_run_id": test_run.get("test_run_id")
     }
     result_url = "{base_url}/buckets/{bucket_key}/tests/{test_id}/results/{test_run_id}".format(**opts)
-    print "Getting result: {}".format(result_url)
+    print("Getting result: {}".format(result_url))
 
     headers = {
         "Authorization": "Bearer {}".format(API_TOKEN),
